7308f1da33429964a291a357699ba5018a8623e8
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-07-01 10:53:50 +0100

    enviroment setup

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,26 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!3y678oerignhnertp');
+  res.send('Hello World!');
+  fetchWithAwait();
 });
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+async function fetchWithAwait(): Promise<void> {
+    const response = await fetch("https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption");
+    const data = await response.json();
+    // deal with JSON response
+    console.log(data);
+  }
+  fetchWithAwait();
